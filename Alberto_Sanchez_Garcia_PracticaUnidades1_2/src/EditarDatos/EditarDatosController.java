82fd8e17a0416fbbae6e04ecea074f43cc3fb489
albertosanchez56 <99281429+albertosanchez56@users.noreply.github.com>
2022-10-24 20:05:22 +0200

    edt3

--- Alberto_Sanchez_Garcia_PracticaUnidades1_2/src/EditarDatos/EditarDatosController.java
+++ Alberto_Sanchez_Garcia_PracticaUnidades1_2/src/EditarDatos/EditarDatosController.java
@@ -6,7 +6,9 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Separator;
 import javafx.scene.control.cell.ComboBoxListCell;
 
 public class EditarDatosController {
@@ -23,10 +25,16 @@ public class EditarDatosController {
 
     @FXML
     private ListView<String> list1;
+    
+    @FXML
+    private ComboBox<Object> tipoCombo;
 
     @FXML
     void initialize() {
 
+    	tipoCombo.setItems(FXCollections.observableArrayList("Consola",new Separator(),"Periferico",new Separator(),"Videojuego"));
+    	tipoCombo.setValue("Select a value...");
+    	
 		list1.setItems(data);
 		list1.setEditable(true);
 		list1.setCellFactory(ComboBoxListCell.forListView(names));
