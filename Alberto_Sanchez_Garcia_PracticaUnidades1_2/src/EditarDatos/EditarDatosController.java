593e6d8dd3a8cc3de066aa9f2b683e1c2c12f149
albertosanchez56 <99281429+albertosanchez56@users.noreply.github.com>
2022-10-24 19:55:49 +0200

    edit2

--- Alberto_Sanchez_Garcia_PracticaUnidades1_2/src/EditarDatos/EditarDatosController.java
+++ Alberto_Sanchez_Garcia_PracticaUnidades1_2/src/EditarDatos/EditarDatosController.java
@@ -1,5 +1,44 @@
 package EditarDatos;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.ComboBoxListCell;
+
 public class EditarDatosController {
 
+	public static final ObservableList<String> names = 
+	        FXCollections.observableArrayList();
+	public static final ObservableList<String> data = 
+	        FXCollections.observableArrayList();
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ListView<String> list1;
+
+    @FXML
+    void initialize() {
+
+		list1.setItems(data);
+		list1.setEditable(true);
+		list1.setCellFactory(ComboBoxListCell.forListView(names));
+		
+		names.addAll(
+	             "Object", "Classes", "Functions", "Variables",
+	             "Compiler", "Debugger", "Projects", "Beans", 
+	             "Libraries", "Modules"
+	        );
+		
+		for (int i = 0; i < 10; i++) {
+            data.add("Indeterminate (pick a choice)");
+        }
+    }
 }
