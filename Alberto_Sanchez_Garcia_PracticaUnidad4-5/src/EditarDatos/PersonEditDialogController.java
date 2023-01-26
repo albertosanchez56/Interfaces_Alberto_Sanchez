0b14ee5dc0e99fe208e6a7175a4c47bdfed48342
albertosanchez56 <99281429+albertosanchez56@users.noreply.github.com>
2023-01-26 19:40:34 +0100

    Add files via upload

---
+++ Alberto_Sanchez_Garcia_PracticaUnidad4-5/src/EditarDatos/PersonEditDialogController.java
@@ -0,0 +1,147 @@
+package EditarDatos;
+import Clases.Persona;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class PersonEditDialogController {
+	 @FXML
+	    private TextField firstName2;
+	    @FXML
+	    private TextField lastName2;
+	    
+	    @FXML
+	    private TextField telefono2;
+	    
+	    @FXML
+	    private TextField direcciones2;
+	    
+	    @FXML
+	    private TextField email2;
+	    
+	    @FXML
+	    private RadioButton femeRadButton2;
+	    
+	    @FXML
+	    private RadioButton mascuRadButton2;
+	    
+	    
+	    @FXML
+	    private TextField puesto2;
+	    
+	    private Stage dialogStage;
+	    private Persona person;
+	    private boolean okClicked = false;
+	    
+	    @FXML
+	    private void initialize() {
+	    	
+	    }
+	    public void setPerson(Persona person) {
+	        this.person = person;
+
+	        firstName2.setText(person.getFirstName());
+	        lastName2.setText(person.getLastName());
+	        telefono2.setText(Integer.toString(person.getTelefono()));
+	        
+	        if(person.getGenero()!=null) {
+	        	if(person.getGenero().equals("masculino")&& person.getGenero()!=null) {
+		        	mascuRadButton2.setSelected(true);
+		        }else if(person.getGenero().equals("femenino")&& person.getGenero()!=null){
+		        	femeRadButton2.setSelected(true);
+		        }
+	        }else {
+	        	mascuRadButton2.setText("masculino");
+	        }
+	        
+	        
+	        puesto2.setText(person.getPuesto());
+	        direcciones2.setText(person.getDireccion());
+	        email2.setText(person.getEmail());
+	       
+	    }
+	    
+	    public boolean isOkClicked() {
+	        return okClicked;
+	    }
+
+	    @FXML
+	    private void handleOk() {
+	    	 if (isInputValid()) {
+	             person.setFirstName(firstName2.getText());
+	             person.setLastName(lastName2.getText());
+	             person.setTelefono(Integer.parseInt(telefono2.getText()));
+	             if(mascuRadButton2.isSelected()) {
+	            	 person.setGenero("masculino");
+	             }else {
+	            	 person.setGenero("femenino");
+	             }
+	            
+	             person.setPuesto(puesto2.getText());
+	             person.setDireccion(direcciones2.getText());
+	             person.setEmail(email2.getText());
+	             
+	             okClicked = true;
+	             dialogStage.close();
+	             
+	         }
+	    }
+	    
+	    
+	    @FXML
+	    private void handleCancel() {
+	        dialogStage.close();
+	    }
+	    
+	    private boolean isInputValid() {
+	    	String errorMessage = "";
+	    	
+	    	if (firstName2.getText() == null || firstName2.getText().length() == 0) {
+	            errorMessage += "El campo nombre está vacío\n"; 
+	        }
+	        if (lastName2.getText() == null || lastName2.getText().length() == 0) {
+	            errorMessage += "El campo apellido está vacío\n"; 
+	        }
+	        if (telefono2.getText() == null || telefono2.getText().length() == 0) {
+	            errorMessage += "El campo telefono está vacío\n"; 
+	        } else {
+	            // Se intenta convertir el código postal en entero y si da un error se muestra un mensaje
+	            try {
+	                Integer.parseInt(telefono2.getText());
+	            } catch (NumberFormatException e) {
+	                errorMessage += "Telefono no válido. Debe ser un número entero\n"; 
+	            }
+	        }
+	        if (direcciones2.getText() == null || direcciones2.getText().length() == 0) {
+	            errorMessage += "El campo direccion está vacío\n"; 
+	        }
+	        if (email2.getText() == null || email2.getText().length() == 0) {
+	            errorMessage += "El campo email está vacío\n"; 
+	        }
+	        if (puesto2.getText() == null || puesto2.getText().length() == 0) {
+	            errorMessage += "El campo puesto está vacío\n"; 
+	        }
+
+	        if (errorMessage.length() == 0) {
+	            return true;
+	        } else {
+	        	// Se muestra un alert si no se puede eliminar la fila
+	    		Alert errorAlert = new Alert(AlertType.ERROR);
+	        	
+	    		errorAlert.setTitle("Hay campos incorrectos");
+	    		errorAlert.setHeaderText("Por favor, rellena correctamente los campos");
+	    		errorAlert.setContentText(errorMessage);
+	    		
+	    		errorAlert.showAndWait();
+	            return false;
+	        }
+	    	
+	    }
+	public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+}
---
+++ Alberto_Sanchez_Garcia_PracticaUnidad4-5/src/application/Main.java
@@ -0,0 +1,156 @@
+package application;
+	
+import java.io.IOException;
+
+import Clases.Persona;
+import Clases.Productos;
+import EditarDatos.EditarDatosController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+		private Stage primaryStage;
+		private BorderPane rootLayout;	
+		private ObservableList<Persona> personData = FXCollections.observableArrayList();
+		private ObservableList<Productos> proData = FXCollections.observableArrayList();
+		
+	
+ 	   public Main() {
+ 			personData.add(new Persona("Alberto", "Sanchez" ,620141444, "C/asdasdasdasdas", "Alberto.smith@example.com","Encargado","masculino"));
+ 			personData.add(new Persona("Lucia", "Garcia" ,51566115, "C/asdasdasdasdas", "Lucia.smith@example.com","Manager","femenino"));
+ 			personData.add(new Persona("Antonio", "Blas" ,51566115, "C/asdasdasdasdas", "Antonio.smith@example.com","Contable","masculino"));
+ 			personData.add(new Persona("Javier", "Escalera" ,51566115, "C/asdasdasdasdas", "Javier.smith@example.com","Encargado","masculino"));
+ 			personData.add(new Persona("Miguel", "Smith" ,51566115, "C/asdasdasdasdas", "Miguel.smith@example.com","Contable","masculino"));
+ 			
+ 			proData.add(new Productos("Play Station 4", "Consola" ,400.0, "China", "00034"));
+ 			proData.add(new Productos("Elden Ring", "Videojuego" ,60.0, "China", "00032"));
+ 			proData.add(new Productos("Logitec 933", "Periferico" ,80.0, "Portugal", "00045"));
+ 			proData.add(new Productos("Skyrim", "Videojuego" ,40.0, "España", "00061"));
+ 			proData.add(new Productos("Xbox 360", "Consola" ,300.0, "España", "00099"));
+ 			
+ 		  
+ 			
+ 			// TODO Probamos el map con solo dos ítems, ya que como se puede observar requiere más líneas de código que la altenrativa anterior
+ 			/*Map<String, Object> item1 = new HashMap<>();
+ 			item1.put("firstName", "Hans");
+ 			item1.put("lastName" , "Muster");
+
+ 			mapData.add(item1);
+
+ 			Map<String, Object> item2 = new HashMap<>();
+ 			item2.put("firstName", "Ruth");
+ 			item2.put("lastName" , "Mueller");
+
+ 			mapData.add(item2);*/
+ 		}
+ 		
+		@Override
+		public void start(Stage primaryStage ) {
+			try {
+				// Carga el diseño del archivo FXML en la variable rootLayout
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(Main.class.getResource("Menu.fxml"));
+				rootLayout = (BorderPane) loader.load();
+				rootLayout.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
+				// Pasamos al controlador de menu el objeto con el BorderPane principal
+				MenuController menuController = loader.getController();
+				menuController.setRootLayout(rootLayout);
+				MenuController main = loader.getController();
+				main.setMainApp(this);
+				
+				// Mostramos la escena del BorderPane de la variable rootLayot
+				Scene scene = new Scene(rootLayout);
+				this.primaryStage = primaryStage;
+				primaryStage.setScene(scene);
+				primaryStage.setResizable(false);
+				
+				
+				//primaryStage.setMaximized(true);
+				primaryStage.setTitle("Practica tema3");
+				primaryStage.show();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		public boolean showPersonEditDialog(Persona person) {
+		    try {
+		        // Cargamos el diseño del diálogo desde un XML
+		        FXMLLoader loader = new FXMLLoader();
+		        loader.setLocation(Main.class.getResource("/EditarDatos/PersonaEditDiealog.fxml"));
+		        AnchorPane page = (AnchorPane) loader.load();
+
+		        // Se crea un nuevo Stage para mostrar el diálogo
+		        Stage dialogStage = new Stage();
+		        dialogStage.setTitle("Crear o editar persona");
+		        dialogStage.initModality(Modality.WINDOW_MODAL);
+		        dialogStage.initOwner(primaryStage);
+		        Scene scene = new Scene(page);
+		        dialogStage.setScene(scene);
+
+		        // Carga la persona en el controlador
+		        EditarDatos.PersonEditDialogController controller = loader.getController();
+		        controller.setDialogStage(dialogStage);
+		        controller.setPerson(person);
+
+		        // Muestra el diálogo y no continúa el código hasta que lo cierra el usuario
+		        dialogStage.showAndWait();
+
+		        return controller.isOkClicked();//controller.isOkClicked();
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		        return false;
+		    }
+		}
+		
+		public boolean showProductEditDialog(Productos product) {
+		    try {
+		        // Cargamos el diseño del diálogo desde un XML
+		        FXMLLoader loader = new FXMLLoader();
+		        loader.setLocation(Main.class.getResource("/EditarDatos/ProductosEditarDialog.fxml"));
+		        AnchorPane page = (AnchorPane) loader.load();
+
+		        // Se crea un nuevo Stage para mostrar el diálogo
+		        Stage dialogStage = new Stage();
+		        dialogStage.setTitle("Crear o editar producto");
+		        dialogStage.initModality(Modality.WINDOW_MODAL);
+		        dialogStage.initOwner(primaryStage);
+		        Scene scene = new Scene(page);
+		        dialogStage.setScene(scene);
+
+		        // Carga la persona en el controlador
+		        EditarDatos.ProductEditDialogController controller = loader.getController();
+		        controller.setDialogStage(dialogStage);
+		        controller.setProduct(product);
+
+		        // Muestra el diálogo y no continúa el código hasta que lo cierra el usuario
+		        dialogStage.showAndWait();
+
+		        return controller.isOkClicked();//controller.isOkClicked();
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		        return false;
+		    }
+		}
+		
+		
+		public static void main(String[] args) {
+			launch(args);
+		}
+		public ObservableList<Persona> getPersonData() {
+			return personData;
+		}
+		public ObservableList<Productos> getProductosData() {
+			return proData;
+		}
+		
+}
